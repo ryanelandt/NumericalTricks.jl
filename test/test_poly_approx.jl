af2777d5d59bc3bb9dbb5da4336ad713a7d6f6ee
Ryan <ryan.elandt@gmail.com>
2019-02-13 22:13:52 -0500

    added soft_clamp test

--- test/test_poly_approx.jl
+++ test/test_poly_approx.jl
@@ -1,4 +1,30 @@
 
+function test_point_bound(p_test, eps_1, clamp_val)
+    n = soft_clamp(p_test - eps_1, clamp_val)
+    z = soft_clamp(p_test,         clamp_val)
+    p = soft_clamp(p_test + eps_1, clamp_val)
+    floating_point_error_allowance = 1.0 + 1.0e-7  # TODO: work on getting this down
+    @test abs(n - z) <= (eps_1 * floating_point_error_allowance)
+    @test abs(p - z) <= (eps_1 * floating_point_error_allowance)
+end
+
+@testset "soft_clamp" begin
+    eps_1 = 1.0e-10
+
+    for scale = (1.0, 2.0)
+        for p_test = [-1.5, -0.5, 0.0, 0.5, 1.5]
+            test_point_bound(p_test * scale, eps_1, 1.0 * scale)
+            test_point_bound(p_test * scale, eps_1, 1.0 * scale)
+        end
+
+        @test -1.0 * scale== soft_clamp(-1.5* scale, 1.0* scale)
+        @test -0.5* scale == soft_clamp(-0.5* scale, 1.0* scale)
+        @test  0.0 * scale== soft_clamp( 0.0* scale, 1.0* scale)
+        @test  0.5 * scale == soft_clamp( 0.5 * scale, 1.0 * scale)
+        @test  1.0 * scale == soft_clamp( 1.5 * scale, 1.0 * scale)
+    end
+end
+
 @testset "fastSigmoid" begin
     for TYPE_k = (Float64, Dual{Nothing,Float64,12})
         x_cut⁻¹ = 1.0
